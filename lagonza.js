f6a04d81ac5d7e2f84b3e4179b8efe46478e035f
achristianonamission <132952751+achristianonamission@users.noreply.github.com>
2023-07-05 19:13:00 -0500

    Add files via upload

---
+++ lagonza.js
@@ -0,0 +1,126 @@
+var lagonzaAutoplay=false 
+document.getElementById("autoplay lagonza").onclick=function(){
+document.getElementById("l l").addEventListener("ended",function(){
+document.getElementById("l vr").play()
+})
+document.getElementById("l vr").addEventListener("ended",function(){
+document.getElementById("l wds ft j").play()
+})
+document.getElementById("l wds ft j").addEventListener("ended",function(){
+document.getElementById("l vdm ft f").play()
+})
+document.getElementById("l vdm ft f").addEventListener("ended",function(){
+document.getElementById("l hp ft eu").play()
+})
+document.getElementById("l hp ft eu").addEventListener("ended",function(){
+document.getElementById("l r ft k").play()
+})
+document.getElementById("l r ft k").addEventListener("ended",function(){
+document.getElementById("ml p ft l").play()
+})
+document.getElementById("ml p ft l").addEventListener("ended",function(){
+document.getElementById("l t").play()
+})
+document.getElementById("l t").addEventListener("ended",function(){
+document.getElementById("l aad").play()
+})
+document.getElementById("l aad").addEventListener("ended",function(){
+document.getElementById("l rf").play()
+})
+document.getElementById("l rf").addEventListener("ended",function(){
+document.getElementById("l b").play()
+})
+document.getElementById("l b").addEventListener("ended",function(){
+document.getElementById("l tef").play()
+})
+document.getElementById("l tef").addEventListener("ended",function(){
+document.getElementById("l smh").play()
+})
+document.getElementById("l smh").addEventListener("ended",function(){
+document.getElementById("l m").play()
+})
+document.getElementById("l m").addEventListener("ended",function(){
+document.getElementById("l aac").play()
+})
+document.getElementById("l aac").addEventListener("ended",function(){
+document.getElementById("l wlf").play()
+})
+document.getElementById("l wlf").addEventListener("ended",function(){
+document.getElementById("l c").play()
+})
+document.getElementById("l c").addEventListener("ended",function(){
+document.getElementById("l wm").play()
+})
+document.getElementById("l wm").addEventListener("ended",function(){
+document.getElementById("l s").play()
+})
+document.getElementById("l s").addEventListener("ended",function(){
+document.getElementById("l ntl").play()
+})
+document.getElementById("l ntl").addEventListener("ended",function(){
+document.getElementById("l m2").play()
+})
+}
+document.getElementById("l l").onplay=function(){
+document.querySelector("title").innerText="Lagonza - Life"
+}
+document.getElementById("l aad").onplay=function(){
+document.querySelector("title").innerText="Lagonza - Angels And Demons"
+}
+document.getElementById("l rf").onplay=function(){
+document.querySelector("title").innerText="Lagonza - Relámpago Furioso"
+}
+document.getElementById("l b").onplay=function(){
+document.querySelector("title").innerText="Lagonza - Believia"
+}
+document.getElementById("l tef").onplay=function(){
+document.querySelector("title").innerText="Lagonza - Tu Eres Fuerte (Prod. Funky)"
+}
+document.getElementById("l smh").onplay=function(){
+document.querySelector("title").innerText="Lagonza - Search My Heart"
+}
+document.getElementById("l m").onplay=function(){
+document.querySelector("title").innerText="Lagonza - Mercy"
+}
+document.getElementById("l aac").onplay=function(){
+document.querySelector("title").innerText="Lagonza - Alone And Crazy"
+}
+document.getElementById("l wlf").onplay=function(){
+document.querySelector("title").innerText="Lagonza - Wanna Live Forever"
+}
+document.getElementById("l c").onplay=function(){
+document.querySelector("title").innerText="Lagonza - Caught"
+}
+document.getElementById("l wm").onplay=function(){
+document.querySelector("title").innerText="Lagonza - Without Me"
+}
+document.getElementById("l s").onplay=function(){
+document.querySelector("title").innerText="Lagonza - Soul"
+}
+document.getElementById("l ntl").onplay=function(){
+document.querySelector("title").innerText="Lagonza - Not To Late"
+}
+document.getElementById("l m2").onplay=function(){
+document.querySelector("title").innerText="Lagonza - Mentiras"
+}
+document.getElementById("l vr").onplay=function(){
+document.querySelector("title").innerText="Lagonza - Vital Reflection"
+}
+document.getElementById("l wds ft j").onplay=function(){
+document.querySelector("title").innerText="Lagonza - We Don't Stop FT JoyBoy"
+}
+document.getElementById("l vdm ft f").onplay=function(){
+document.querySelector("title").innerText="Lagonza - Valle De Muerte FT Flaquitopa"
+}
+document.getElementById("l hp ft eu").onplay=function(){
+document.querySelector("title").innerText="Lagonza - Hecho Pedazos ft EDU URBAHNO"
+}
+document.getElementById("l r ft k").onplay=function(){
+document.querySelector("title").innerText="Lagonza - Resist FT KMINO"
+}
+document.getElementById("ml p ft l").onplay=function(){
+document.querySelector("title").innerText="Micah Lynee - Persistance ft Lagonza"
+}
+document.getElementById("l t").onplay=function(){
+document.querySelector("title").innerText="Lagonza - Time"
+}
